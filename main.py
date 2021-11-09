855f8ab062f2baeab48489bb975dc5fb8ae46170
nadavof <93834717+nadavof@users.noreply.github.com>
2021-11-09 22:44:20 +0200

    add docker file
    update files with bug fixes

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-#from typing_extensions import Required
+
 import numpy as np
-from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
-import requests
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+
 
 app = Flask(__name__)
 api = Api(app)
 
+#DB SIZE LIMITATIONs
+LIMITATION = 10000
+PORT = 5000
 
-
+#POST arguments
 person_post_args = reqparse.RequestParser()
 person_post_args.add_argument("name", type=str, required=True,  help="name of the person")
 person_post_args.add_argument("features", action='append', required=True, help="person features vector")
 
+#GET arguments
 person_get_args = reqparse.RequestParser()
 person_get_args.add_argument("features", action='append', required=True, help="person features vector")
 
@@ -31,7 +34,6 @@ def get_closest(db, input_person):
     for name, feature in db.items():
         feature = np.array(feature).astype(np.float)
         if np.array_equal(feature,input_person):
-            print('YOU ARE YOU, skip', name)
             continue
       #  print('KEY', name)
      #   print('VAL == INPUT' , feature, input_person)
@@ -46,9 +48,10 @@ def get_closest(db, input_person):
             print(e)
             abort(500, message = "[ERROR] calculating closest person")
     top_3 = sorted(res, key=lambda d: d['distance'], reverse=True)[:3]
-    print(f"top 3 are: {top_3}")
-    print([(d['name'], d['distance']) for d in top_3])
-    return [(d['name'], d['distance']) for d in top_3]
+  #  print(f"top 3 are: {top_3}")
+   # print(db[name])
+   # print([(d['name'], d['distance']) for d in top_3])
+    return [(d['name'], db[name]) for d in top_3]
 
 def is_not_in_db(name):
     if name not in persons_data:
@@ -60,8 +63,9 @@ def person_exist(name):
 
 
 def limitation():
-    if len(persons_data) > 10000:
-        abort(404, message = "[ERROR] Cannot add new person - DB is full.")
+    if len(persons_data) > LIMITATION:
+        return True
+
 
 class Person(Resource):
     def get(self, name):
@@ -74,7 +78,10 @@ class Person(Resource):
         try:
             args = person_post_args.parse_args()
             #check 10K limitation
-            limitation()
+            if limitation():
+
+                return 'DB IS FULL', 403
+
             name = args["name"]
             # check if person already exist in DB
             person_exist(name)
@@ -83,7 +90,7 @@ class Person(Resource):
             return 'SUCESS', 201
         except:
             return 'FAIL', 400
-        #return {"data": "Posted Hello World"} 
+
 
     def delete(self, name):
         is_not_in_db(name)
@@ -91,86 +98,11 @@ class Person(Resource):
         # Deleted succesfully 
         return '', 204
 
-#api.add_resource(Person, "/person")
+
 api.add_resource(Person, "/person/<string:name>")
-#args = person_post_args.parse_args()
-#api.add_resource(Person, '/person',  resource_class_kwargs={ 'name': args.name, 'features': args.feature})
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# db = SQLAlchemy(app)
-
-# class VideoModel(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	name = db.Column(db.String(100), nullable=False)
-# 	views = db.Column(db.Integer, nullable=False)
-# 	likes = db.Column(db.Integer, nullable=False)
-
-# 	def __repr__(self):
-# 		return f"Video(name = {name}, views = {views}, likes = {likes})"
-
-# video_put_args = reqparse.RequestParser()
-# video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-# video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
-# video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
-
-# video_update_args = reqparse.RequestParser()
-# video_update_args.add_argument("name", type=str, help="Name of the video is required")
-# video_update_args.add_argument("views", type=int, help="Views of the video")
-# video_update_args.add_argument("likes", type=int, help="Likes on the video")
-
-# resource_fields = {
-# 	'id': fields.Integer,
-# 	'name': fields.String,
-# 	'views': fields.Integer,
-# 	'likes': fields.Integer
-# }
-
-# class Video(Resource):
-# 	@marshal_with(resource_fields)
-# 	def get(self, video_id):
-# 		result = VideoModel.query.filter_by(id=video_id).first()
-# 		if not result:
-# 			abort(404, message="Could not find video with that id")
-# 		return result
-
-# 	@marshal_with(resource_fields)
-# 	def put(self, video_id):
-# 		args = video_put_args.parse_args()
-# 		result = VideoModel.query.filter_by(id=video_id).first()
-# 		if result:
-# 			abort(409, message="Video id taken...")
-
-# 		video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-# 		db.session.add(video)
-# 		db.session.commit()
-# 		return video, 201
-
-# 	@marshal_with(resource_fields)
-# 	def patch(self, video_id):
-# 		args = video_update_args.parse_args()
-# 		result = VideoModel.query.filter_by(id=video_id).first()
-# 		if not result:
-# 			abort(404, message="Video doesn't exist, cannot update")
-
-# 		if args['name']:
-# 			result.name = args['name']
-# 		if args['views']:
-# 			result.views = args['views']
-# 		if args['likes']:
-# 			result.likes = args['likes']
-
-# 		db.session.commit()
-
-# 		return result
-
-
-# 	def delete(self, video_id):
-# 		abort_if_video_id_doesnt_exist(video_id)
-# 		del videos[video_id]
-# 		return '', 204
-
-
-#api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
+    #app.run(debug=True, host="0.0.0.0")
+   # app.run(host="0.0.0.0", port=PORT)
--- post.py
+++ post.py
@@ -1,92 +1,29 @@
 import requests
 import numpy as np
-import pandas as pd
+
 
 BASE = "http://127.0.0.1:5000/"
-FEATURES_LENGHT = 3 #256
+FEATURES_LENGHT = 256
  
 NAMES_EXAMPLE = ["Orly", "Yoram", "Matan", "Nadav", "Stav", "Chen", "Dauber", "Amrani", "Markman", "Ron"]
-#NAMES_EXAMPLE = ["Orly", "Yoram", "Matan", "Nadav", "Stav"]
+
 
 data =[]
-# a = np.array([1,2,3])
-# b = np.array([1,3,3])
-# c = np.array([3,4,17])
-# d = np.array([1,16,13])
-# e = np.array([2,3,4])
 
 f = np.array([0.98,0.92,0.91])
 
-#np_arrays = [a, b, c, d, e]
+
 
 #Generating random features
 for i, name in enumerate(NAMES_EXAMPLE):
     data.append({"name": name, "features" : np.random.rand(FEATURES_LENGHT)})
     #data.append({"name": name, "features": np_arrays[i]})
-    print(f"Adding to DB person: {data[i]}")
+    #print(f"Adding to DB person: {data[i]}")
 
     respose = requests.delete(BASE + f"person/{name}")
 
 for i in range(len(data)):
     #generating persons & features pair
-    #print('This data was sent:', str(i), data[i])
     respose = requests.post(BASE + "person/" + str(i), data[i])
     print(respose.json())
-
-
-# print('GET API:')
-# respose = requests.get(BASE + f"person/Yoram", {'features': f})
-# print(respose.json())
-
-
-
-
-# def dot_product(data_test, search):
-#     res = []
-#     for key, val in data_test.items():
-#         res.append({"name" :  key, "distance": np.dot(search/search.sum(), val/val.sum())})
-#     newlist = sorted(res, key=lambda d: d['distance'], reverse=True)
-#     return newlist
-# a = np.array([1,2,3])
-# norm_a = np.linalg.norm(a)
-# normal_array_a = a/norm_a
-#
-# b = np.array([1,2,3])
-# norm_b = np.linalg.norm(b)
-# normal_array_b = b/norm_b
-# print(np.dot(normal_array_b, normal_array_a))
-#
-# b = np.array([1,5,10])
-# c = np.array([1,15,16])
-# d = np.array([1,6,13])
-# e = np.array([1,5,12])
-#
-# #s= pd.Series([1,5,12])
-#
-# np_arrays = [a, b, c, d]
-# data_test = {}
-# for i, name in enumerate(['A', 'B', 'C', 'D']):
-#     data_test[name] = np_arrays[i]
-#
-#
-# #df = pd.DataFrame.from_dict(data_test)
-# #df = pd.DataFrame.from_dict(data_test, dtype='ndarray')
-#
-# # for index, row in df.iterrows():
-# #     df['dot_product'][index] = np.dot(e, row['features'][index])
-#
-# res = dot_product(data_test, e)
-#
-# ############
-# def get_closest(db, input_person):
-#     res = []
-#     for key, val in db.items():
-#         res.append({"name" :  key, "distance": np.dot(input_person/input_person.sum(), val/val.sum())})
-#     top_3 = sorted(res, key=lambda d: d['distance'], reverse=True)[:3]
-#     return [d['name'] for d in top_3]
-#
-#
-# ############
-#
-# n=1
 
